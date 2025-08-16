6fc074f097cd83c16a502e321811c69fd0a21aa2
shouta totsuka <5h0utat0t2uka@gmail.com>
2025-08-16 15:27:17 +0900

    update

--- dot_config/nvim/lua/plugins/statuscol.lua
+++ dot_config/nvim/lua/plugins/statuscol.lua
@@ -1,39 +1,70 @@
 return {
-  "luukvbaal/statuscol.nvim",
-  event = "VeryLazy",
-  opts = function()
-    local builtin = require("statuscol.builtin")
-    return {
-      setopt = true,
-      relculright = true,
-      segments = {
-        -- { text = { "~ " } },
-        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-        -- Diagnostics
-        {
-          sign = {
-            name = { "Diagnostic" },
-            maxwidth = 2,
-            colwidth = 1,
-            auto = true,
-            wrap = false,
+  {
+    'luukvbaal/statuscol.nvim',
+    event = 'BufReadPre',
+    config = function()
+      local builtin = require('statuscol.builtin')
+      require('statuscol').setup({
+        bt_ignore = { 'terminal', 'nofile', 'ddu-ff', 'ddu-ff-filter' },
+
+        relculright = true,
+        segments = {
+          {
+            sign = {
+              namespace = { 'diagnostic/signs' },
+              maxwidth = 1,
+            },
           },
-          click = "v:lua.ScSa",
-        },
-        -- GitSigns
-        {
-          sign = {
-            namespace = { "gitsigns" },
-            maxwidth = 1,
-            colwidth = 1,
-            auto = true,
-            wrap = false,
-            fillchar = " ",
+          {
+            sign = {
+              namespace = { 'gitsigns' },
+              maxwidth = 1,
+              colwidth = 1,
+              wrap = true,
+              fillchar = " ",
+            },
           },
-          click = "v:lua.ScSa",
+          { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          { text = { '  ' } },
         },
-        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-      },
-    }
-  end,
+      })
+    end,
+  },
 }
+
+-- return {
+--   "luukvbaal/statuscol.nvim",
+--   event = "VeryLazy",
+--   opts = function()
+--     local builtin = require("statuscol.builtin")
+--     return {
+--       setopt = true,
+--       relculright = true,
+--       segments = {
+--         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+--         {
+--           sign = {
+--             name = { "Diagnostic" },
+--             maxwidth = 2,
+--             colwidth = 1,
+--             auto = true,
+--             wrap = false,
+--           },
+--           click = "v:lua.ScSa",
+--         },
+--         {
+--           sign = {
+--             namespace = { "gitsigns" },
+--             maxwidth = 1,
+--             colwidth = 1,
+--             auto = true,
+--             wrap = false,
+--             fillchar = " ",
+--           },
+--           click = "v:lua.ScSa",
+--         },
+--         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+--       },
+--     }
+--   end,
+-- }
