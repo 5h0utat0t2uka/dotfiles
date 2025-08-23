6b9edcfa082837d074bcbbe3a32557b9606aa496
shouta totsuka <5h0utat0t2uka@gmail.com>
2025-08-23 20:10:30 +0900

    update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,6 +9,7 @@ vim.opt.expandtab = true
 vim.opt.wildmenu = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 5
 -- vim.opt.cmdheight = 0
 vim.opt.showmatch = true
 vim.opt.cursorline = true
