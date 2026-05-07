af8368bd09b74efea6f841af1646b90aab67e266
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-05-07 16:47:35 +0900

    add cache settings

--- nix/flake.nix
+++ nix/flake.nix
@@ -64,6 +64,24 @@
         };
         modules = [
           # ----------------------------------------------------
+          # キャッシュ
+          # ----------------------------------------------------
+          {
+            nix.settings = {
+              substituters = [
+                "https://cache.nixos.org"
+                "https://nix-community.cachix.org"
+                "https://5h0uta.cachix.org"
+              ];
+              trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBo="
+                "5h0uta.cachix.org-1:YrFB/6azmFXnXmmosBRBwB3lEKBpUq7Hn+tJvzTqtGc="
+              ];
+            };
+          }
+
+          # ----------------------------------------------------
           # nixpkgs 設定
           # ----------------------------------------------------
           {
