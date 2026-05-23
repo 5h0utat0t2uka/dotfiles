bf345782d494a238459d857c4df9f9ab3242b81d
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-05-23 22:28:17 +0900

    fix(pass): change default editor from neovim to vim in pass script

--- nix/modules/home-manager/pass/default.nix
+++ nix/modules/home-manager/pass/default.nix
@@ -4,12 +4,12 @@ let
   passWithOtp = pkgs.pass.withExtensions (exts: [
     exts.pass-otp
   ]);
-  pass-nvim = pkgs.writeShellScriptBin "pass" ''
-    exec env EDITOR=${pkgs.neovim}/bin/nvim VISUAL=${pkgs.neovim}/bin/nvim ${passWithOtp}/bin/pass "$@"
+  pass-editor = pkgs.writeShellScriptBin "pass" ''
+    exec env EDITOR=vim VISUAL=vim ${passWithOtp}/bin/pass "$@"
   '';
 in
 {
   home.packages = [
-    pass-nvim
+    pass-editor
   ];
 }
