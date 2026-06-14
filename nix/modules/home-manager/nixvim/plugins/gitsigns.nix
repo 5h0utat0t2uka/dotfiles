3409d03c0ba5a3ff53b0d7c4c45626a4d35a0753
5h0utat0t2uka <5h0utat0t2uka@gmail.com>
2026-06-15 05:19:31 +0900

    fix(nixvim): update gitsigns highlight settings for better visibility

--- nix/modules/home-manager/nixvim/plugins/gitsigns.nix
+++ nix/modules/home-manager/nixvim/plugins/gitsigns.nix
@@ -71,12 +71,19 @@
         end
       '';
     };
-    highlight = {
-      GitSignsAdd.fg = "#A3BE8C";
-      GitSignsChange.fg = "#EBCB8B";
-      GitSignsDelete.fg = "#BF616A";
-      GitSignsChangedelete.fg = "#D08770";
-      GitSignsUntracked.fg = "#B48EAD";
-    };
+    # highlight = {
+    #   GitSignsAdd.fg = "#A3BE8C";
+    #   GitSignsChange.fg = "#EBCB8B";
+    #   GitSignsDelete.fg = "#BF616A";
+    #   GitSignsChangedelete.fg = "#D08770";
+    #   GitSignsUntracked.fg = "#B48EAD";
+    # };
+  };
+  highlightOverride = {
+    GitSignsAdd.fg = "#A3BE8C";
+    GitSignsChange.fg = "#EBCB8B";
+    GitSignsDelete.fg = "#BF616A";
+    GitSignsChangedelete.fg = "#D08770";
+    GitSignsUntracked.fg = "#B48EAD";
   };
 }
